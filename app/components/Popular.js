45bbfa9dc6a3ec3f4559d624eeb8bf2207e827f8
Michael D. Perini <perinimichaeld@yahoo.com>
2020-01-04 11:44:33 -0500

    code update - card and tooltip usage

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { fetchPopularRepos} from '../utils/api';
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa';
+import Card from './Card'
 import Loading from './Loading'
+import Tooltip from './Tooltip'
 
 const LanguagesNav = ({ selected, onUpdateLangauge }) => {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python', 'HTML'];
@@ -36,43 +38,45 @@ function RepoGrid ({ repos }) {
                const { login, avatar_url } = owner
 
                return (
-                   <li key={html_url} className='repo bg_light'>
-                       <h4 className='header-lg center-text'>
-                           #{index + 1 }
-                       </h4>
-                       <img
-                        className='avatar'
-                        src={avatar_url}
-                        alt={`Avatar for ${login}`}
-                        />
-                        <h2 className='center-text'>
-                            <a className='link' href={html_url}>{login}</a>
-                        </h2> 
-                        <ul className='card-list'>
-                            <li>
-                              <FaUser color='rgb(255, 191, 116)' size={22} /> 
-                              <a href={`https://github.com/${login}`}>
-                                  {login}
-                              </a> 
-                            </li>
-                            <li>
-                                <FaStar color='rgb(255, 215, 0)' size={22} />
-                                {stargazers_count.toLocaleString()} stars
-                            </li>
-                            <li>
-                                <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
-                                {forks.toLocaleString()} forks
-                            </li>
-                            <li>
-                               <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} /> 
-                               {open_issues.toLocaleString()} open
-                            </li>
-                        </ul> 
+                   <li key={html_url}>
+                       <Card
+                            header={`#${ index + 1 }`}
+                            avatar={avatar_url}
+                            href={html_url}
+                            name={login}
+                            >
+                            <ul className='card-list'>
+                                <li>
+                                    <Tooltip text="Github username">
+                                        <FaUser color='rgb(255, 191, 116)' size={22} />
+                                        <a href={`https://github.com/${login}`}>
+                                            {login}
+                                        </a>
+                                    </Tooltip>
+                                </li>
+                                <li>
+                                    <FaStar color='rgb(255, 215, 0)' sixe='22' />
+                                    {stargazers_count.toLocaleString()} stars
+                                </li>
+                                <li>
+                                    <FaCodeBranch color='rgb(129, 195, 245)' size='22' />
+                                    {forks.toLocaleString()} forks
+                                </li>
+                                <li>
+                                    <FaExclamationTriangle color='rgb(241, 138, 147)' size='22' />
+                                    {open_issues.toLocaleString()} open
+                                </li>
+                            </ul>
+                        </Card>
                    </li>
                )
            })}
        </ul> 
     )
+}
+ 
+RepoGrid.protoTypes = {
+    repos: PropTypes.array.isRequiredß
 }
 
 export default class Popular extends React.Component {
@@ -109,7 +113,6 @@ export default class Popular extends React.Component {
                 })
             }
         }
-    
         isLoading = () => {
             const {selectedLanguage, repos, error } = this.state;
             return !repos[selectedLanguage] && error === null
@@ -129,8 +132,7 @@ export default class Popular extends React.Component {
         
                     {error && <p>{error}</p>}
 
-                    {repos[selectedLanguage] && 
-                        <RepoGrid repos={repos[selectedLanguage]} />}
+                    {repos[selectedLanguage] && <RepoGrid repos={repos[selectedLanguage]} />}
                 </React.Fragment>
             )
         }
