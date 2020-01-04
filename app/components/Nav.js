b5492665735aa38a5041ec54bc02a615102570aa
Michael D. Perini <perinimichaeld@yahoo.com>
2020-01-04 10:43:21 -0500

    completed

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ThemeConsumer } from '../contexts/theme'
+import ThemeContext from '../contexts/theme'
 import { NavLink } from 'react-router-dom'
 
 const activeStyle = {
@@ -7,9 +7,9 @@ const activeStyle = {
 }
 
 export default function Nav ({ toggleTheme }) {
+  const theme = React.useContext(ThemeContext)
+
   return (
-    <ThemeConsumer>
-      {(theme) => (
         <nav className='row space-between'>
           <ul className='row nav'>
             <li>
@@ -38,7 +38,5 @@ export default function Nav ({ toggleTheme }) {
             {theme === 'light' ? '🔦' : '💡'}
           </button>
         </nav>
-      )}
-    </ThemeConsumer>
   )
 }
